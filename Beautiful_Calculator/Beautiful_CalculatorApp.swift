fa08822665273182037ef7d418aa4d07b7c10c95
Ryunosuke-yo <ryokokawa00@gmail.com>
2022-10-26 23:57:15 -0700

    Initial Commit

---
+++ Beautiful_Calculator/Beautiful_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Beautiful_CalculatorApp.swift
+//  Beautiful_Calculator
+//
+//  Created by Ryunosuke Yokokawa on 2022-10-26.
+//
+
+import SwiftUI
+
+@main
+struct Beautiful_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
